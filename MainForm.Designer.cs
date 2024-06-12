880dc872336b8d0a0abcaddd7df1d26b18b1ea78
V-Vik <V-Vik@Z370N-WIFI>
2024-06-12 08:52:02 +0200

    Projektdateien hinzufügen.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DiskPart_Control_Assistant
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Main_treeView = new System.Windows.Forms.TreeView();
+            this.Update_Disks_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Main_treeView
+            // 
+            this.Main_treeView.Location = new System.Drawing.Point(12, 41);
+            this.Main_treeView.Name = "Main_treeView";
+            this.Main_treeView.Size = new System.Drawing.Size(283, 397);
+            this.Main_treeView.TabIndex = 0;
+            // 
+            // Update_Disks_button
+            // 
+            this.Update_Disks_button.Location = new System.Drawing.Point(12, 12);
+            this.Update_Disks_button.Name = "Update_Disks_button";
+            this.Update_Disks_button.Size = new System.Drawing.Size(283, 23);
+            this.Update_Disks_button.TabIndex = 1;
+            this.Update_Disks_button.Text = "&Update Disks";
+            this.Update_Disks_button.UseVisualStyleBackColor = true;
+            this.Update_Disks_button.Click += new System.EventHandler(this.Update_Disks_button_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Update_Disks_button);
+            this.Controls.Add(this.Main_treeView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "DiskPart Control Assistant";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView Main_treeView;
+        private System.Windows.Forms.Button Update_Disks_button;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace DiskPart_Control_Assistant
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Initialize UI components
+            this.Text = "DiskPart Control Assistant";
+            Update_Disks_button.Text = "Update Disks";
+            Main_treeView.Nodes.Clear();
+        }
+
+        private void Update_Disks_button_Click(object sender, EventArgs e)
+        {
+            UpdateDiskList();
+        }
+
+        private void UpdateDiskList()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "diskpart.exe";
+                process.StartInfo.Arguments = "/s scriptListe.txt";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;;
+                process.Start();
+;
+                File.WriteAllText("scriptListe.txt", "list disk");
+
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+
+
+
+
+                //// Weiterverarbeitung des Outputs
+                //ParseDiskListOutput(output);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void ParseDiskListOutput(string output)
+        {
+            Main_treeView.Nodes.Clear();
+            string[] lines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("  Disk"))
+                {
+                    string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length > 2 && parts[1] != "0") // Exclude Disk 0
+                    {
+                        TreeNode diskNode = new TreeNode($"Disk {parts[1]} - {parts[3]} {parts[4]}");
+                        diskNode.Tag = parts[1]; // Store disk number for later use
+                        Main_treeView.Nodes.Add(diskNode);
+
+                        // Now retrieve partitions for this disk
+                        RetrieveAndAddPartitions(diskNode);
+                    }
+                }
+            }
+        }
+
+        private void RetrieveAndAddPartitions(TreeNode diskNode)
+        {
+            string diskNumber = diskNode.Tag.ToString();
+
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "diskpart.exe";
+                process.StartInfo.Arguments = $"/c select disk {diskNumber} & list part";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                string[] lines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith("  Partition"))
+                    {
+                        string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (parts.Length > 2)
+                        {
+                            TreeNode partNode = new TreeNode($"{parts[1]} - {parts[2]} {parts[3]} {parts[4]}");
+                            diskNode.Nodes.Add(partNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
